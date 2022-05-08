99c2e2224ffac4f10ffa55bee738126828aa9967
carterwsheppard <carterwsheppard@gmail.com>
2022-05-08 11:24:22 -0400

    initial commit for dev branch - HTML draft is ready, CSS draft is ready, JS psuedocoded (partially) taking a break then rethinking through psuedocode and moving forward

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+//INITIAL VARIABLES
+
+var score = 60;
+var questions = [
+    
+    {
+    prompt:
+    answer:
+},
+
+{
+    prompt:
+    answer:
+},
+
+{
+    prompt:
+    answer:
+},
+
+{
+    prompt:
+    answer:
+},
+
+{
+    prompt:
+    answer:
+},
+];
+
+
+
+
+//SET TIMER TO COUNT DOWN FUNCTION
+
+var startTimer = function() {
+
+};
+
+
+//SET FUNCTION TO SWAP THE CURRENT PROMPT FOR THE NEXT PROMPT
+
+var 
+
+
+//INCLUDE LOGIC FOR ADDING TO OR SUBTRACTING FROM SCORE BASED ON RESPONSE MATCH, 
+
+
+
+
+
+//MASTER FUNCTION STRUCTURE: starting with the first event click on a button - 1)run start timer to begin countdown 2) enter the function with loop of questions
