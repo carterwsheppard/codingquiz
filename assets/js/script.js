7923fa881cd6960df57c10f0394dd532cb1e7346
carterwsheppard <carterwsheppard@gmail.com>
2022-05-08 20:00:13 -0400

    draft 1 created - error with when the answer is

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,112 @@
 //INITIAL VARIABLES
-
-var score = 60;
+var aBtn = document.querySelector("#a");
+var bBtn = document.querySelector("#b");
+var cBtn = document.querySelector("#c");
+var dBtn = document.querySelector("#d");
+var scoreEl = document.querySelector("#time");
+var btnzEl = document.querySelector("#btnz");
+var questionPrompt = document.querySelector("#questionPrompt");
+var currentScore = 60;
+var i = 0;
 var questions = [
     
     {
-    prompt:
-    answer:
+        prompt: "What does CSS stand for? \n(a) Carter's Styling Stuff\n(b) Cascading Style Sheet\n(c) Coding Style Sheet\n(d) None of the Above"
+        ,answer:"b"
 },
 
 {
-    prompt:
-    answer:
-},
+    prompt: "What does HTML stand for? \n(a) Carter's Styling Stuff\n(b) Cascading Style Sheet\n(c) Coding Style Sheet\n(d) None of the Above"
+    ,answer:"c"
+}
+]
 
-{
-    prompt:
-    answer:
-},
+// INITIATE QUIZ AND TIMER
+var quizStart = function() {
+    var btnID = event.target.getAttribute("id");
+    if (i===0) {
+        questionPrompt.textContent = questions[i].prompt;
+        questionAnswer = questions[i].answer;
+        i++
+//set timer to count down
+var startTimer = setInterval(function(event) {
+    if (currentScore >= 1) {
+      currentScore--;
+      scoreEl.textContent = currentScore;
+    } 
+    else {
+      scoreEl.textContent = '';
+      clearInterval(startTimer);
+      window.alert("Sorry - you're out of time! Hit refresh to try again.")
+      return false
+    }
+    if (i === questions.length) {
+        clearInterval(startTimer);
+    }
+  }, 1000);
 
-{
-    prompt:
-    answer:
-},
-
-{
-    prompt:
-    answer:
-},
-];
+        return false
+            };
+        };
 
 
+//MAIN QUIZ FUNCTION
 
-
-//SET TIMER TO COUNT DOWN FUNCTION
-
-var startTimer = function() {
-
+var quizTaking = function() {
+if (i < questions.length && currentScore > 0) {
+// get the id of the button click
+var btnID = event.target.getAttribute("id");
+var questionAnswer = questions[i-1].answer;
+console.log("questionanswer" + questionAnswer)
+questionPrompt.textContent = questions[i].prompt;
+   if (btnID === questionAnswer) {
+        currentScore = currentScore + 10;
+        scoreEl.textContent = currentScore;
+        return false
+    };
+    if (btnID === !questionAnswer) {
+        currentScore = currentScore - 10;
+        scoreEl.textContent = currentScore;
+        return false
+    };
+    if (currentScore < 0) {
+        window.prompt("Sorry - you're out of time! Hit refresh to try again.")
+    }
+    i++;
+}
 };
 
+//QUIZ ENDING AT LAST QUESTION FUNCTION
 
-//SET FUNCTION TO SWAP THE CURRENT PROMPT FOR THE NEXT PROMPT
-
-var 
-
-
-//INCLUDE LOGIC FOR ADDING TO OR SUBTRACTING FROM SCORE BASED ON RESPONSE MATCH, 
-
-
+var quizOver = function() {
+    var btnID = event.target.getAttribute("id");
+    finalScore = currentScore;
+    scoreEl.textContent = finalScore;
+    questionPrompt.textContent = "Congratulations - you have finished the quiz with a score of " + currentScore + ".";
+}
 
 
+//MASTER FUNCTION:
+
+var mainFunction = function() {
+    var btnID = event.target.getAttribute("id");
+    if (i === 0) {
+        quizStart();
+    }
+    else if (i === questions.length) {
+        quizOver();
+    }
+    else {
+        quizTaking();
+}
+console.log(btnID);
+};
 
 //MASTER FUNCTION STRUCTURE: starting with the first event click on a button - 1)run start timer to begin countdown 2) enter the function with loop of questions
+console.log(questions[i].answer)
+console.log(questions[i].prompt)
+
+aBtn.addEventListener("click",mainFunction);
+bBtn.addEventListener("click",mainFunction);
+cBtn.addEventListener("click",mainFunction);
+dBtn.addEventListener("click",mainFunction);
